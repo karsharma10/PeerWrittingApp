ff3531a686fd813b9a7edf2977952050b17a4ac3
Kartik Sharma <kash5740@colorado.edu>
2023-12-21 17:45:22 -0700

    added socket io, nodemon, and create states for our texteditor

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -1,13 +1,30 @@
-import React, {useCallback} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css" //import quil style sheet
+import {io} from "socket.io-client";
+
 export default function TextEditor() {
+    const [socket, setSocket] = useState()
+    const [quil, setQuil] = useState()
+    useEffect(()=>{
+        const urlSocket = io("http://localhost:3001") //this will return a socket, this is the backend URL
+        setSocket(urlSocket)
+        return() =>{
+            urlSocket.disconnect() //when we want to cleanup, we want to disconnect the socket.
+        }
+    },[])
+
+
+    
+
+
     const wrapperRef = useCallback(wrapper => {
         if (wrapper == null) return
         wrapper.innerHTML = ""
         const editor = document.createElement('div')
         wrapper.append(editor)
-        new Quill(editor, {theme: "snow"})
+        const createQuil = new Quill(editor, {theme: "snow"})
+        setQuil(createQuil)
     }, [])
     return (
         <div className="container" ref={wrapperRef}></div>
--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const io = require('socket.io')(3001, { //pass the port you want to run it on
-    cors:{
+    cors: {
         origin: "http://localhost:3000",  //where we are want to receive, so we want to use cors for frontend and backend
-        methods: ["GET", "POST"] //what methods we want it to use. 
+        methods: ["GET", "POST"] //what methods we want it to use.
     }
 })
+
+io.on("connection",  socket =>{
+    console.log("connected")
+}) //everytime our io connect it will run this connection
