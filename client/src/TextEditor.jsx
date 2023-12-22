90e52cd81878296c7f1e073f4490890462a845d4
Kartik Sharma <kash5740@colorado.edu>
2023-12-21 18:59:13 -0700

    added mongodb

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -2,10 +2,12 @@ import React, {useCallback, useEffect, useState} from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css" //import quil style sheet
 import {io} from "socket.io-client";
-
+import {useParams} from "react-router-dom";
 export default function TextEditor() {
     const [socket, setSocket] = useState()
     const [quil, setQuil] = useState()
+    const {id: documentId} = useParams()
+
     useEffect(()=>{
         const urlSocket = io("http://localhost:3001") //this will return a socket, this is the backend URL
         setSocket(urlSocket)
@@ -47,12 +49,28 @@ export default function TextEditor() {
     }, [socket, quil]);
 
 
+   //send the people to the corresponding 'rooms' so we don't have the changes being broadcasted to all documents:
+    useEffect(()=>{
+        if(socket == null || quil == null) return //if its empty we don't want to do anything
+
+        socket.once("load-document", document=>{
+            quil.setContents(document)
+            quil.enable()
+        })
+        socket.emit("get-document", documentId)
+
+
+    },[socket, quil, documentId])
+
+
     const wrapperRef = useCallback(wrapper => {
         if (wrapper == null) return
         wrapper.innerHTML = ""
         const editor = document.createElement('div')
         wrapper.append(editor)
         const createQuil = new Quill(editor, {theme: "snow"})
+        createQuil.disable()
+        createQuil.setText('Loading...')
         setQuil(createQuil)
     }, [])
     return (
--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+const mongoose = require("mongoose")
+
+
+await mongoose.connect('mongodb://localhost/peerwritting');
+
 const io = require('socket.io')(3001, { //pass the port you want to run it on
     cors: {
         origin: "http://localhost:3000",  //where we are want to receive, so we want to use cors for frontend and backend
@@ -6,8 +11,14 @@ const io = require('socket.io')(3001, { //pass the port you want to run it on
 })
 
 io.on("connection",  socket =>{
-    socket.on("send-changes", delta =>{
-        socket.broadcast.emit("receive-changes", delta) //on this socket, broadcast to everyone else that there are changes being made except for us.
+    socket.on("get-document", documentId =>{
+        const data = ""
+        socket.join(documentId)
+        socket.emit("load-document", data)
+        socket.on("send-changes", delta =>{
+            socket.broadcast.to(documentId).emit("receive-changes", delta) //on this socket, broadcast to everyone else that there are changes being made except for us.
+        })
     })
+
     console.log("connected")
 }) //everytime our io connect it will run this connection
