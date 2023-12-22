f67181cf9b46e4c39d4273d40fdeb3abe6ab23a9
Kartik Sharma <kash5740@colorado.edu>
2023-12-21 17:20:01 -0700

    Added cors to connect frontend and backend

---
+++ server/server.js
@@ -0,0 +1,6 @@
+const io = require('socket.io')(3001, { //pass the port you want to run it on
+    cors:{
+        origin: "http://localhost:3000",  //where we are want to receive, so we want to use cors for frontend and backend
+        methods: ["GET", "POST"] //what methods we want it to use. 
+    }
+})
